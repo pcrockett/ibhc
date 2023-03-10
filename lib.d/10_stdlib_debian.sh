cbee8b1b0936c4c2c1a466ecbb66edc07e392068
Philip Crockett <contact@philcrockett.com>
2023-03-10 21:28:53 +0100

    Add package_is_installed and install_deb for debian

--- lib.d/10_stdlib_debian.sh
+++ lib.d/10_stdlib_debian.sh
@@ -5,3 +5,20 @@
 apt_install() {
     sudo apt-get install --yes "${@}"
 }
+
+package_is_installed() {
+    local pkg_name="${1}"
+    local desired_version="${2:-}"
+
+    if [ "${desired_version}" == "" ]; then
+        dpkg --status "${pkg_name}" &> /dev/null
+    else
+        dpkg --status "${pkg_name}" \
+            | grep --line-regexp --fixed-strings "Version: ${desired_version}" &> /dev/null
+    fi
+}
+
+install_deb() {
+    local deb_file_path="${1}"
+    sudo dpkg --install "${deb_file_path}"
+}
